a2e746379a8e48a6b01b854a17b6455af3f7ae60
aadil124 <ansariaadil124@gmail.com>
2023-06-26 13:06:26 +0530

    added functionality for repo btn

--- githubapi.js
+++ githubapi.js
@@ -1,10 +1,12 @@
 const btnUser = document.querySelector("#getUser");
 const showUserResult = document.querySelector(".showUserData");
 const btnRepo = document.querySelector("#getRepo");
+const showRepoResult = document.querySelector(".showRepoData");
 
+// Functionality for User data results
 btnUser.addEventListener("click", async () => {
   const inputValue = document.querySelector("input").value;
-  showUserResult.innerHTML = `Loading data for ${inputValue}`;
+//   showUserResult.innerHTML = `Loading data for ${inputValue} ...`;
   //   console.log(inputValue);
   try {
     let res = await fetch(`https://api.github.com/users/${inputValue}`);
@@ -12,7 +14,7 @@ btnUser.addEventListener("click", async () => {
     // console.log(userData);
     displayUserData(userData);
   } catch (error) {
-    console.log(error);
+    showUserResult.innerHTML = `Error in Loading the data...`;
   }
 });
 
@@ -26,7 +28,7 @@ const displayUserData = (userData) => {
   const userFollowing = userData.following;
   const userRepo = userData.public_repos;
   showUserResult.innerHTML = `
-<div class="col">
+<div class="col-sm-12 col-md-6 col-lg-4 mb-3 mb-sm-3 ">
             <div class="card h-100 bg-dark text-white">
             <div class = "card-header text-center">
                 <img src="${userImg}" class="card-img-top" width="200" height="300" alt="${userName}">
@@ -41,6 +43,41 @@ const displayUserData = (userData) => {
                 </div>
             </div>
 </div>
-
 `;
+  document.querySelector("input").value = "";
+};
+
+// Functionality for Repo results
+btnRepo.addEventListener("click", async () => {
+  const inputValue = document.querySelector("input").value;
+//   showUserResult.innerHTML = `Loading Repo for ${inputValue} ...`;
+  try {
+    let res = await fetch(`https://api.github.com/users/${inputValue}/repos`);
+    let userData = await res.json();
+    displayRepoData(userData);
+  } catch (error) {
+    showUserResult.innerHTML = `Error in loading repositories...`;
+  }
+});
+
+const displayRepoData = (userData) => {
+  userData.map((item, id) => {
+    // console.log(item, id);
+    showUserResult.innerHTML += `
+ <div class="col-sm-12 col-md-6 col-lg-4 mb-3 mb-sm-3 ">
+            <div class="card h-100 bg-dark text-white text-center">
+            <div class = "card-header">
+                <h6>Repository No: ${id + 1}</h6>
+                <h3 class="mt-2">${item.name}</h3>
+            </div>
+                <div class="card-body">
+                <a href="${item.html_url}" target="_blank">
+                <button class="btn btn-outline-warning p-2">Go To Repo</button></a>
+                </div>
+            </div>
+</div>
+`;
+  });
+
+  document.querySelector("input").value = "";
 };
